3ecda34d312cc0c31b714e7a7dd1f774a9cdb83b
PROBREAKVS <100469370+PROBREAKVS@users.noreply.github.com>
2024-04-10 12:04:10 +0300

    Delete download.sh

--- download.sh
+++
@@ -1,3 +0,0 @@
- 
-
-sudo apt-get install --download-only plasma-widgets-addons plasma-workspace plasma-workspace-wallpapers plasma-systemmonitor gwenview firefox-esr python3-minimal konsole dolphin sddm xwayland flatpak snapd fonts-hack vlc nano neofetch kcalc pulseaudio pipewire discover kate libcurl4 libxml2 fonts-dejavu fonts-liberation ttf-mscorefonts-installer fonts-crosextra-carlito fonts-takao-gothic fonts-opensymbol
